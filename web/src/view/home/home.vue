48d330c2fd1afd8a1885cbe787ca0cd150d2a422
lizhi <lizhilz88_v@didichuxing.com>
2018-11-23 15:00:53 +0800

    修改

--- web/src/view/home/home.vue
+++ web/src/view/home/home.vue
@@ -21,14 +21,14 @@
                         </Select>
                       </FormItem>
                     </Col>
-                    <!-- <Col span="8">
+                    <Col span="8">
                       <FormItem label="是否在职">
                         <Select v-model="formTop.isJob" placeholder="是否在职">
                           <Option :value="1">在职</Option>
                           <Option :value="0">离职</Option>
                         </Select>
                       </FormItem>
-                    </Col> -->
+                    </Col>
                     <!-- <Col span="8">
                       <FormItem label="面试时间">
                         <div class="cl">
@@ -69,7 +69,7 @@
                           <Option :value="'4'">设计师</Option>
                         </Select>
                       </FormItem>
-                    </Col> 
+                    </Col>
                     <!-- <Col span="8">
                       <FormItem label="是否转正">
                         <Select v-model="formTop.isBecome" placeholder="是否转正">
@@ -105,16 +105,15 @@
             v-model="modal1"
             className="vertical-center-model"
             :title="TitleText"
-            width="800"
-            @on-ok="ADDok"
-            @on-cancel="ADDcancel">
+            :mask-closable="false"
+            width="800">
             <Form :model="addFrom" ref="addFrom" label-position="top" style="padding-left: 3%; padding-top: 1%;">
                  <Row>
                     <Col span="8">
                       <FormItem label="姓名">
                         <Input v-model="addFrom.userName" placeholder="请输入姓名"></Input>
                       </FormItem>
-                      
+
                     </Col>
                     <Col span="8">
                       <FormItem label="是否面试通过">
@@ -173,7 +172,7 @@
                           <Option :value="4">设计师</Option>
                         </Select>
                       </FormItem>
-                    </Col> 
+                    </Col>
                     <Col span="8">
                       <FormItem label="是否转正">
                         <Select v-model="addFrom.isBecome" placeholder="是否转正">
@@ -181,22 +180,26 @@
                           <Option :value="0">否</Option>
                         </Select>
                       </FormItem>
-                    </Col> 
+                    </Col>
                     <Col span="24">
                       <FormItem label="备注">
                         <Input v-model="addFrom.Remarks" type="textarea" :rows="3" placeholder="请输入备注"></Input>
                       </FormItem>
-                    </Col> 
+                    </Col>
                   </Row>
             </Form>
+            <div slot="footer">
+                <Button type="text" size="large" @click="ADDcancel">取消</Button>
+                <Button type="primary" size="large" @click="ADDok">确定</Button>
+            </div>
         </Modal>
-
 
         <!--详情-->
         <Modal
             v-model="modal2"
             title="详情"
             width="800"
+            :mask-closable="false"
             >
             <Row v-if="modal2" class="datail-box">
                     <Col span="8">
@@ -234,9 +237,9 @@
                    <Col span="8">
                         <i>职位：</i>
                         <span>
-                            {{ 
-                                detailModelData.jobPosition == 0 ? 
-                                    'web前端工程师' 
+                            {{
+                                detailModelData.jobPosition == 0 ?
+                                    'web前端工程师'
                                 : detailModelData.jobPosition == 1 ?
                                     'java工程师'
                                 : detailModelData.jobPosition == 2 ?
@@ -245,15 +248,15 @@
                                     '产品经理'  : '设计师'
                              }}
                         </span>
-                    </Col> 
+                    </Col>
                     <Col span="8">
                         <i>是否转正：</i>
                         <span>{{detailModelData.isBecome == 1 ? '是' : '否'}}</span>
-                    </Col> 
+                    </Col>
                     <Col span="24">
                         <i>备注：</i>
                         <span>{{detailModelData.Remarks}}</span>
-                    </Col> 
+                    </Col>
                   </Row>
         </Modal>
     </div>
@@ -263,269 +266,279 @@
 import { workUserAdd, workUserGetList, workUserUpdata } from '@/api/data.js'
 import { getDateToHtml } from '@/libs/tools.js'
 export default {
-  name: "lawperson",
+  name: 'lawperson',
   components: {},
-  data() {
+  data () {
     return {
       getDateToHtml,
       TitleText: '新增',
       loading: true,
       pageNum: 1, // 当前页面
       pageSize: 10, // 每页显示条数
-      totals: "", // 总数
+      totals: '', // 总数
       data: [],
       formTop: {
         userName: null, // 姓名
         phone: null, // 手机号
-        isThrough: null,// 面试是否通过
+        isThrough: null, // 面试是否通过
         isJob: null, // 是否在职
         InterviewTimer_Start: null, // 面试时间-开始
         InterviewTimer_End: null, // 面试结束-时间
         EntryTimer_Start: null, // 入职时间-开始时间
-        EntryTimer_End: null , // 入职结束时间
+        EntryTimer_End: null, // 入职结束时间
         isBecome: null, // 是否转正
-        jobPosition: null, // 工作职位
+        jobPosition: null // 工作职位
       },
       // 新增对象
-      addFrom:{
-          userName: null, // 姓名
-          phone: null, // 手机号
-          isThrough: null,// 面试是否通过
-          isJob: null, // 是否在职
-          InterviewTimer: null, // 面试时间
-          EntryTimer: null, // 入职时间
-          isBecome: null, // 是否转正
-          jobPosition: null, // 工作职位
-          salary: null, // 薪资
-          Expect_salary: null, // 期望薪资
-          Remarks: null, // 备注
+      addFrom: {
+        userName: null, // 姓名
+        phone: null, // 手机号
+        isThrough: null, // 面试是否通过
+        isJob: null, // 是否在职
+        InterviewTimer: null, // 面试时间
+        EntryTimer: null, // 入职时间
+        isBecome: null, // 是否转正
+        jobPosition: null, // 工作职位
+        salary: null, // 薪资
+        Expect_salary: null, // 期望薪资
+        Remarks: null // 备注
       },
-      list: [], //修改
+      list: [], // 修改
       columns1: [
-         {
-          title: "姓名",
-          align: "center",
-          key: "userName",
-        },
-         {
-          title: "手机号",
-          align: "center",
-          key:'phone',
+        {
+          title: '姓名',
+          align: 'center',
+          key: 'userName'
         },
         {
-          title: "职位",
-          align: "center",
+          title: '手机号',
+          align: 'center',
+          key: 'phone'
+        },
+        {
+          title: '职位',
+          align: 'center',
           render: (h, params) => {
             if (params.row.jobPosition === 0) {
-              return h("div", {}, "web前端工程师");
+              return h('div', {}, 'web前端工程师')
             } else if (params.row.jobPosition == 1) {
-              return h("div", {}, "java工程师");
-            }else if (params.row.jobPosition == 2){
-              return h("div", {}, "测试工程师");
-            }else if (params.row.jobPosition == 3) {
-              return h("div", {}, "产品经理");
-            }else if (params.row.jobPosition == 4){
-              return h("div", {}, "设计师");
+              return h('div', {}, 'java工程师')
+            } else if (params.row.jobPosition == 2) {
+              return h('div', {}, '测试工程师')
+            } else if (params.row.jobPosition == 3) {
+              return h('div', {}, '产品经理')
+            } else if (params.row.jobPosition == 4) {
+              return h('div', {}, '设计师')
             }
           }
         },
         {
-          title: "面试时间",
-          align: "center",
-          key: "InterviewTimer",
+          title: '面试时间',
+          align: 'center',
+          key: 'InterviewTimer',
           render: (h, params) => {
-                return h("div", {}, getDateToHtml(params.row.InterviewTimer).split(' ')[0]);
-          }     
-        },
-         {
-          title: "是否通过",
-          align: "center",
-          key: "isThrough",
-          render: (h, params) => {
-              if (params.row.isThrough === 0) {
-                return h("Tag", { props: { color:"warning"}}, "面试不通过");
-              } else {
-                return h("Tag", { props: { color:"success"}}, "通过");
-              }
+            return h('div', {}, getDateToHtml(params.row.InterviewTimer).split(' ')[0])
           }
         },
         {
-            // 
-          title: "操作",
-          align: "center",
+          title: '是否通过',
+          align: 'center',
+          key: 'isThrough',
           render: (h, params) => {
-            return h("div", [
-              h("Icon", {
+            if (params.row.isThrough === 0) {
+              return h('Tag', { props: { color: 'warning'}}, '面试不通过')
+            } else {
+              return h('Tag', { props: { color: 'success'}}, '通过')
+            }
+          }
+        },
+        {
+          //
+          title: '操作',
+          align: 'center',
+          render: (h, params) => {
+            return h('div', [
+              h('Icon', {
                 attrs: {
-                  type: "ios-eye-outline",
-                  style: "font-size:26px;cursor: pointer; margin-left:8px;margin-top: 4px;"
+                  type: 'ios-eye-outline',
+                  style: 'font-size:26px;cursor: pointer; margin-left:8px;margin-top: 4px;'
                 },
                 on: {
                   click: () => {
-                      this.modal2 = true;
-                      this.detailModelData = {
-                          ...params.row
-                      }
+                    this.modal2 = true
+                    this.detailModelData = {
+                      ...params.row
+                    }
                   }
                 }
               }),
-              h("Icon", {
+              h('Icon', {
                 attrs: {
-                  type: "ios-create-outline",
-                  style: "font-size:22px;cursor: pointer; margin-left:8px"
+                  type: 'ios-create-outline',
+                  style: 'font-size:22px;cursor: pointer; margin-left:8px'
                 },
                 on: {
                   click: () => {
-                      this.TitleText = '编辑'
-                      let { 
-                            id,
-                            userName, // 姓名
-                            phone, // 手机号
-                            isThrough,// 面试是否通过
-                            isJob, // 是否在职
-                            InterviewTimer, // 面试时间
-                            EntryTimer, // 入职时间
-                            isBecome, // 是否转正
-                            jobPosition, // 工作职位
-                            salary, // 薪资
-                            Expect_salary, // 期望薪资
-                            Remarks, // 备注
-                        } = params.row
-                      this.addFrom = {
-                            id,
-                            userName, // 姓名
-                            phone, // 手机号
-                            isThrough,// 面试是否通过
-                            isJob, // 是否在职
-                            InterviewTimer, // 面试时间
-                            EntryTimer, // 入职时间
-                            isBecome, // 是否转正
-                            jobPosition, // 工作职位
-                            salary, // 薪资
-                            Expect_salary, // 期望薪资
-                            Remarks, // 备注
-                      },
-                      this.modal1=true; 
+                    this.TitleText = '编辑'
+                    let {
+                      id,
+                      userName, // 姓名
+                      phone, // 手机号
+                      isThrough, // 面试是否通过
+                      isJob, // 是否在职
+                      InterviewTimer, // 面试时间
+                      EntryTimer, // 入职时间
+                      isBecome, // 是否转正
+                      jobPosition, // 工作职位
+                      salary, // 薪资
+                      Expect_salary, // 期望薪资
+                      Remarks // 备注
+                    } = params.row
+                    this.addFrom = {
+                      id,
+                      userName, // 姓名
+                      phone, // 手机号
+                      isThrough, // 面试是否通过
+                      isJob, // 是否在职
+                      InterviewTimer, // 面试时间
+                      EntryTimer, // 入职时间
+                      isBecome, // 是否转正
+                      jobPosition, // 工作职位
+                      salary, // 薪资
+                      Expect_salary, // 期望薪资
+                      Remarks // 备注
+                    },
+                    this.modal1 = true
                   }
                 }
               })
-            ]);
+            ])
           }
         }
       ],
       detailModel: false,
       detailModelData: null,
       modal1: false,
-      modal2: false,
-    };
+      modal2: false
+    }
   },
-  created() {
+  created () {
     // 初始化
-    this.init();
+    this.init()
   },
-  mounted() {},
+  mounted () {},
   computed: {
-     
+
   },
   methods: {
-    //重置
-    resetBtn() {
-      this.formTop =  {
+    // 重置
+    resetBtn () {
+      this.formTop = {
         userName: null, // 姓名
         phone: null, // 手机号
-        isThrough: null,// 面试是否通过
+        isThrough: null, // 面试是否通过
         isJob: null, // 是否在职
         InterviewTimer_Start: null, // 面试时间-开始
         InterviewTimer_End: null, // 面试结束-时间
         EntryTimer_Start: null, // 入职时间-开始时间
-        EntryTimer_End: null , // 入职结束时间
+        EntryTimer_End: null, // 入职结束时间
         isBecome: null, // 是否转正
-        jobPosition: null, // 工作职位
+        jobPosition: null // 工作职位
       }
     },
     // 获取分页
-    getList() {
+    getList () {
       let data = {
         index: this.pageNum,
         rows: this.pageSize,
         ...this.formTop
-      };
-      this.loading = true;
+      }
+      this.loading = true
       workUserGetList(data).then(res => {
-           let data = res.data;
-           if(data.errno === '0'){
-               this.list = data.data.rows;
-               this.totals = data.data.count;
-               this.pageNum = data.data.offset;
-            }
-           setTimeout( () => {
-              this.loading = false;
-           },500)
+        let data = res.data
+        if (data.errno === '0') {
+          this.list = data.data.rows
+          this.totals = data.data.count
+          this.pageNum = data.data.offset
+        }
+        setTimeout(() => {
+          this.loading = false
+        }, 500)
       })
     },
     // 初始化
-    init() {
-      this.getList();
+    init () {
+      this.getList()
     },
     // 分页切换
-    getpage(ev) {
-      this.pageNum = ev;
-      this.getList();
+    getpage (ev) {
+      this.pageNum = ev
+      this.getList()
     },
-    getpagesize(ev) {
-      this.pageSize = ev;
-      this.getList();
+    getpagesize (ev) {
+      this.pageSize = ev
+      this.getList()
     },
-    sortChange() {},
+    sortChange () {},
     //  开始时间
-    InterviewTimerStart(ev) {
-      this.$set(this.formTop, "InterviewTimer_Start", ev);
+    InterviewTimerStart (ev) {
+      this.$set(this.formTop, 'InterviewTimer_Start', ev)
     },
     //  结束时间
-    InterviewTimerEnd(ev) {
-      this.$set(this.formTop, "InterviewTimer_End", ev);
+    InterviewTimerEnd (ev) {
+      this.$set(this.formTop, 'InterviewTimer_End', ev)
     },
-    //查看
-    detailModelOpen() {
-      this.detailModel = true;
+    // 查看
+    detailModelOpen () {
+      this.detailModel = true
     },
     // 打开新增
-    openModel1(){
-        this.TitleText = '新增'
-        this.addFrom = {
-          userName: null, // 姓名
-          phone: null, // 手机号
-          isThrough: null,// 面试是否通过
-          isJob: null, // 是否在职
-          InterviewTimer: null, // 面试时间
-          EntryTimer: null, // 入职时间
-          isBecome: null, // 是否转正
-          jobPosition: null, // 工作职位
-          salary: null, // 薪资
-          Expect_salary: null, // 期望薪资
-          Remarks: null, // 备注
-        },
-        this.modal1=true; 
+    openModel1 () {
+      this.TitleText = '新增'
+      this.addFrom = {
+        userName: null, // 姓名
+        phone: null, // 手机号
+        isThrough: null, // 面试是否通过
+        isJob: null, // 是否在职
+        InterviewTimer: null, // 面试时间
+        EntryTimer: null, // 入职时间
+        isBecome: null, // 是否转正
+        jobPosition: null, // 工作职位
+        salary: null, // 薪资
+        Expect_salary: null, // 期望薪资
+        Remarks: null // 备注
+      },
+      this.modal1 = true
     },
     // 新增确定
-    ADDok(ev) {
-        if(this.addFrom.id) {
-            workUserUpdata(this.addFrom).then( res => {
-                this.init();
-            })
-        } else {
-            workUserAdd(this.addFrom).then( res => {
-                this.init();
-            })
-        }
+    ADDok (ev) {
+      if (this.addFrom.id) {
+        workUserUpdata(this.addFrom).then(res => {
+          if (res.data.errno === '0') {
+            this.init()
+            this.modal1 = false
+          } else {
+            this.$Message.warning('更新失败！')
+          }
+        })
+      } else {
+        workUserAdd(this.addFrom).then(res => {
+          if (res.data.errno === '0') {
+            this.init()
+            this.modal1 = false
+          } else {
+            this.$Message.warning('添加成功！')
+          }
+        })
+      }
     },
     // 新增关闭
-    ADDcancel(ev){
+    ADDcancel (ev) {
     },
-    detailSubmit() {},
-    handleReset() {}
+    detailSubmit () {},
+    handleReset () {}
   }
-};
+}
 </script>
 <style scoped lang='less'>
 .ivu-card {
@@ -562,5 +575,3 @@ export default {
     padding-left: 6px;
 }
 </style>
-
-
